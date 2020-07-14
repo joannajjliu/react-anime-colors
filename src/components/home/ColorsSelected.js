9ecec28525d6da6604fcaec6c54393e019512bf3
joannajjliu <joanna.liu3774@gmail.com>
2020-07-14 00:15:47 -0400

    colors selected conditional rendering and styling

--- src/components/home/ColorsSelected.js
+++ src/components/home/ColorsSelected.js
@@ -1,9 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styles from "./ColorsSelected.module.css";
 
-export const ColorsSelected = (props) => (
-  <h1>The colors you have selected are {props.selectedColors.join(", ")}</h1>
-);
+export const ColorsSelected = (props) => {
+  const selectedColors = props.selectedColors;
+  if (selectedColors && selectedColors.length > 0) {
+    let returnString = [];
+    {
+      selectedColors.map((selectedColor, index) => {
+        returnString.push(
+          <div
+            key={index}
+            className={`${styles.square}`}
+            style={{ backgroundColor: `#${selectedColor}` }}
+          >
+            <div className={`${styles.squareText}`}>{selectedColor}</div>
+          </div>
+        );
+      });
+    }
+    return (
+      <div>
+        <h1>The colors you have selected are:</h1>
+        {returnString}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Please select up to 3 colors.</h1>
+      </div>
+    );
+  }
+};
 
 ColorsSelected.propTypes = {
   selectedColors: PropTypes.array,
